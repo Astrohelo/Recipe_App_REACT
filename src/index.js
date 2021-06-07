1c32637439284d4b2483bcb62d91a7ab385413ce
Astrohelo <58273598+Astrohelo@users.noreply.github.com>
2021-06-08 01:11:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ChakraProvider } from "@chakra-ui/react"
+
+
 ReactDOM.render(
   
   <React.StrictMode>
+    <ChakraProvider>
     <App></App>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
